d248adccd8630d01c104af2518cbc49d989bea4b
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-18 12:24:08 -0300

    npm install package e import modules/services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { InsumosModule } from './insumos/insumos.module';
-import { CategoriasModule } from './categorias/categorias.module';
+import { InsumosModule } from './modules/insumos/insumos.module';
+import { CategoriasModule } from './modules/categorias/categorias.module';
 
 @Module({
   imports: [InsumosModule, CategoriasModule],
