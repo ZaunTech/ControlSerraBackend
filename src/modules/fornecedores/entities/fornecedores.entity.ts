aa1484307b1a2e58c18c32e9101fc639f323e782
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-19 12:55:11 -0300

    feature: atributos para dtos e entity Fornecedores

--- src/modules/fornecedores/entities/fornecedores.entity.ts
+++ src/modules/fornecedores/entities/fornecedores.entity.ts
@@ -1,1 +1,24 @@
-export class Fornecedores {}
+import { contaTipo } from "@prisma/client"
+
+export class Fornecedores {
+    id: number
+    email: string
+    telefone: string
+    contaTipo: contaTipo
+    rua?:       string 
+    numero?:    string
+    complemento?: string
+    cep?:         string
+    bairro?:      string
+    cidade?:      string
+    estado?:      string
+    pais?:        string
+    Nome?:        string
+    cpf?:         string
+    rg?:          string
+    nomeFantasia?: string
+    razaoSocial?: string
+    cnpj?:        string
+    createdAt: Date
+    updatedAt: Date
+}
