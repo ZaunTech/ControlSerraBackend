aa65246efc3b8ab05ede9d2e5df9aa0563ae4f5f
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-19 16:26:55 -0300

    update: atributos dto e entity produto

--- src/modules/produtos/entities/produto.entity.ts
+++ src/modules/produtos/entities/produto.entity.ts
@@ -1,1 +1,13 @@
-export class Produto {}
+import { produtoTipo } from "@prisma/client";
+import { ListaInsumo } from "../../lista-insumos/entities/lista-insumo.entity";
+
+export class Produto {
+    id: number;
+    titulo: string;
+    quantidade?: number;
+    valorUnitario?: number;
+    tipo: produtoTipo;
+    listaInsumos: ListaInsumo;
+    createdAt: Date;
+    updatedAt: Date;
+}
