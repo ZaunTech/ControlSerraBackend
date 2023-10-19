e92080cfd527d92d9b0cb07140e6faf8961c3d55
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-19 16:26:42 -0300

    update: atributos dto e entity pedido

--- src/modules/pedidos/dto/update-pedido.dto.ts
+++ src/modules/pedidos/dto/update-pedido.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePedidoDto } from './create-pedido.dto';
+import { status } from "@prisma/client";
+import { Orcamento } from "src/modules/orcamentos/entities/orcamento.entity";
 
-export class UpdatePedidoDto extends PartialType(CreatePedidoDto) {}
+export class UpdatePedidoDto extends PartialType(CreatePedidoDto) {
+    pagamento?: number;
+    status?: status;
+    idOrcamento?: number;
+    orcamentos?: Orcamento;
+}
