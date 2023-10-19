38d6c17e262122dde81de97cf23470e088010d58
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-19 16:27:08 -0300

    update: atributos dto e entity usuario

--- src/modules/usuarios/dto/create-usuario.dto.ts
+++ src/modules/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUsuarioDto {}
+import { tipoUsuario } from "@prisma/client";
+import { Orcamento } from "../../orcamentos/entities/orcamento.entity";
+
+export class CreateUsuarioDto {
+    tipoUsuario: tipoUsuario;
+    nome: string;
+    cpf: string;
+    email: string;
+    telefone: string;
+    senha: string;
+    idOrcamento?: number;
+    orcamento?: Orcamento
+}
