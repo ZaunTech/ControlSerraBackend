8fa2505aed2652885f07f4aff54c7ef79a940407
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-19 14:06:41 -0300

    update: atributos dto e entity cliente

--- src/modules/clientes/dto/create-cliente.dto.ts
+++ src/modules/clientes/dto/create-cliente.dto.ts
@@ -1,1 +1,21 @@
-export class CreateClienteDto {}
+import { contaTipo } from "@prisma/client";
+
+export class CreateClienteDto {
+    email: string;
+    telefone: string;
+    contaTipo: contaTipo;
+    rua?:       string ;
+    numero?:    string;
+    complemento?: string;
+    cep?:         string;
+    bairro?:      string;
+    cidade?:      string;
+    estado?:      string;
+    pais?:        string;
+    Nome?:        string;
+    cpf?:         string;
+    rg?:          string;
+    nomeFantasia?: string;
+    razaoSocial?: string;
+    cnpj?:        string;
+}
--- src/modules/clientes/dto/update-cliente.dto.ts
+++ src/modules/clientes/dto/update-cliente.dto.ts
@@ -1,4 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClienteDto } from './create-cliente.dto';
+import { contaTipo } from '@prisma/client';
 
-export class UpdateClienteDto extends PartialType(CreateClienteDto) {}
+export class UpdateClienteDto extends PartialType(CreateClienteDto) {
+    email?: string;
+    telefone?: string;
+    contaTipo?: contaTipo;
+    rua?:       string ;
+    numero?:    string;
+    complemento?: string;
+    cep?:         string;
+    bairro?:      string;
+    cidade?:      string;
+    estado?:      string;
+    pais?:        string;
+    Nome?:        string;
+    cpf?:         string;
+    rg?:          string;
+    nomeFantasia?: string;
+    razaoSocial?: string;
+    cnpj?:        string;
+}
