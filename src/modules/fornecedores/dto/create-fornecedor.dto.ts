d9708da356420906c1a5ddd05589d3e1e0a01833
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-20 21:11:40 -0300

    update: dtos create update fornecedor

--- src/modules/fornecedores/dto/create-fornecedor.dto.ts
+++ src/modules/fornecedores/dto/create-fornecedor.dto.ts
@@ -1,21 +1,23 @@
 import { contaTipo } from "@prisma/client"
+import { Cotacao } from "../../cotacaos/entities/cotacao.entity"
 
 export class CreateFornecedorDto{
-    email: string
-    telefone: string
-    contaTipo: contaTipo
-    rua?:       string 
-    numero?:    string
-    complemento?: string
-    cep?:         string
-    bairro?:      string
-    cidade?:      string
-    estado?:      string
-    pais?:        string
-    Nome?:        string
-    cpf?:         string
-    rg?:          string
+    email: string;
+    telefone: string;
+    contaTipo: contaTipo;
+    pais?:        string;
+    cep?:         string;
+    estado?:      string;
+    cidade?:      string;
+    bairro?:      string;
+    rua?:       string ;
+    numero?:    string;
+    complemento?: string;
+    nome?:        string;
+    cpf?:         string;
+    rg?:          string;
     nomeFantasia?: string
     razaoSocial?: string
     cnpj?:        string
+    cotacaos?:    Cotacao[];
 }
--- src/modules/fornecedores/dto/update-fornecedor.dto.ts
+++ src/modules/fornecedores/dto/update-fornecedor.dto.ts
@@ -1,23 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFornecedorDto } from './create-fornecedor.dto';
 import { contaTipo } from '@prisma/client';
+import { Cotacao } from '../../cotacaos/entities/cotacao.entity';
 
 export class UpdateFornecedorDto extends PartialType(CreateFornecedorDto) {
-    email?: string
-    telefone?: string
-    contaTipo?: contaTipo
-    rua?:       string 
-    numero?:    string
-    complemento?: string
-    cep?:         string
-    bairro?:      string
-    cidade?:      string
-    estado?:      string
-    pais?:        string
-    Nome?:        string
-    cpf?:         string
-    rg?:          string
+    email?: string;
+    telefone?: string;
+    contaTipo?: contaTipo;
+    pais?:        string;
+    cep?:         string;
+    estado?:      string;
+    cidade?:      string;
+    bairro?:      string;
+    rua?:       string ;
+    numero?:    string;
+    complemento?: string;
+    nome?:        string;
+    cpf?:         string;
+    rg?:          string;
     nomeFantasia?: string
     razaoSocial?: string
     cnpj?:        string
+    cotacaos?:    Cotacao[];
 }
