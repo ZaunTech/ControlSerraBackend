2d8eed1793c95dbd4c51a8fcc4c888b35cbb9b70
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-24 12:58:32 -0300

    update: class validator  usuario dtos

--- src/modules/usuarios/dto/create-usuario.dto.ts
+++ src/modules/usuarios/dto/create-usuario.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { tipoUsuario } from '@prisma/client';
+import { IsEnum, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUsuarioDto {
   @ApiProperty({
@@ -7,6 +8,7 @@ export class CreateUsuarioDto {
       'O tipo de usuario serve para descrever o nivel de acesso dele',
     example: 'Vendedor',
   })
+  @IsEnum(tipoUsuario)
   tipoUsuario: tipoUsuario;
 
   @ApiProperty({
@@ -14,17 +16,27 @@ export class CreateUsuarioDto {
       'O nome do usuário serve para identificar e pesquisar o usuário',
     example: 'Sérgio Moraes',
   })
+  @IsString()
   nome: string;
 
   @ApiProperty({
     description: 'O CPF serve para identificar o usuario',
     example: '02370334029',
   })
+  @IsString()
+  @MaxLength(11,{message: "Limite Maximo de caracteres para CPF"})
+  @Matches(/(?<=\D|^)(\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}|\d{3}\.?\d{3}\.?\d{3}-?\d{2})(?=\D|$).*$/,{
+        message:"CPF Invalido"
+  })
   cpf: string;
 
   @ApiProperty({
     description: 'O email serve para descrever o email do usuario',
     example: 'email@gmail.com',
+  })
+  @IsString()
+  @Matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,{
+    message: "Insira um endereco de email valido"
   })
   email: string;
 
@@ -39,6 +51,12 @@ export class CreateUsuarioDto {
     description:
       'A senha serve para o usuário realizar o acesso dentro da aplicação',
     example: '1234',
+  })
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+      message: 'senha muito fraca',
   })
   senha: string;
 }
--- src/modules/usuarios/dto/update-usuario.dto.ts
+++ src/modules/usuarios/dto/update-usuario.dto.ts
@@ -2,6 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
 import { tipoUsuario } from "@prisma/client";
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
     @ApiProperty({
@@ -9,6 +10,7 @@ export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
           'O tipo de usuario serve para descrever o nivel de acesso dele',
         example: 'Vendedor',
       })
+      @IsEnum(tipoUsuario)
       tipoUsuario?: tipoUsuario;
     
       @ApiProperty({
@@ -16,17 +18,27 @@ export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
           'O nome do usuário serve para identificar e pesquisar o usuário',
         example: 'Sérgio Moraes',
       })
+      @IsString()
       nome?: string;
     
       @ApiProperty({
         description: 'O CPF serve para identificar o usuario',
         example: '02370334029',
       })
+      @IsString()
+      @MaxLength(11,{message: "Limite Maximo de caracteres para CPF"})
+      @Matches(/(?<=\D|^)(\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}|\d{3}\.?\d{3}\.?\d{3}-?\d{2})(?=\D|$).*$/,{
+        message:"CPF Invalido"
+      })
       cpf?: string;
     
       @ApiProperty({
         description: 'O email serve para descrever o email do usuario',
         example: 'email@gmail.com',
+      })
+      @IsString()
+      @Matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,{
+      message: "Insira um endereco de email valido"
       })
       email?: string;
     
@@ -35,12 +47,19 @@ export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
           'O telefone serve para descrever o numero de telefone do usuario',
         example: '1734112736',
       })
+      @IsString()
       telefone?: string;
     
       @ApiProperty({
         description:
           'A senha serve para o usuário realizar o acesso dentro da aplicação',
         example: '1234',
+      })
+      @IsString()
+      @MinLength(8)
+      @MaxLength(20)
+      @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'senha muito fraca. A senha precisa de 8 a 20 caracteres',
       })
       senha?: string;
 }
