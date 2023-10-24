5f59c3b6a2f9208e0209da54451ee2e5ecb19b32
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-24 12:58:08 -0300

    update: class validator  cliente dtos

--- src/modules/clientes/dto/create-cliente.dto.ts
+++ src/modules/clientes/dto/create-cliente.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { contaTipo } from '@prisma/client';
+import { IsEnum, IsString, Matches, ValidateIf } from 'class-validator';
 
 export class CreateClienteDto {
   @ApiProperty({
     description: 'O email serve para descrever o email do cliente',
     example: 'email@gmail.com',
+  })
+  @IsString()
+  @Matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,{
+    message: "Insira um endereco de email valido"
   })
   email: string;
 
@@ -12,59 +17,78 @@ export class CreateClienteDto {
     description: 'O telefone serve para descrever o numero de telefone do cliente',
     example: '1734112736',
   })
+  @IsString()
   telefone: string;
 
   @ApiProperty({
     description: 'O tipo serve para diferenciar entre pessoa fisica e juridica',
     example: 'Fisica',
   })
+  @IsEnum(contaTipo)
   contaTipo: contaTipo;
 
   @ApiProperty({
     description: 'O nome serve para identificar o cliente, caso seja pessoa fisica',
     example: 'João Pedro',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Fisica)
+  @IsString()
   nome?: string;
 
   @ApiProperty({
     description: 'O CPF serve para identificar o cliente, caso seja pessoa fisica',
     example: '02370334029',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Fisica)
+  @IsString()
   cpf?: string;
 
   @ApiProperty({
     description: 'O RG serve para identificar o cliente, caso seja pessoa fisica',
     example: '114421225',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Fisica)
+  @IsString()
   rg?: string;
 
   @ApiProperty({
     description: 'O nome fantasia serve para identificar o cliente, caso seja pessoa juridica',
     example: 'ZawnTech',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Juridica)
+  @IsString()
   nomeFantasia?: string;
 
   @ApiProperty({
     description: 'A razão social serve para identificar o cliente, caso seja pessoa juridica',
     example: 'Industria mecanica modelo Ltda.',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Juridica)
+  @IsString()
   razaoSocial?: string;
 
   @ApiProperty({
     description: 'O CNPJ serve para identificar o cliente, caso seja pessoa juridica',
     example: '31895255000193',
   })
+  @ValidateIf((o) => o.contaTipo === contaTipo.Juridica)
+  @IsString()
   cnpj?: string;
 
   @ApiProperty({
     description: 'O pais serve para identificar a região onde o cliente se encontra',
     example: 'Brasil',
   })
+  @IsString()
   pais?: string;
 
   @ApiProperty({
     description: 'O CEP serve para identificar a região onde o cliente se encontra',
     example: '69918170',
+  })
+  @IsString()
+  @Matches(/^([\d]{2})\.?([\d]{3})\-?([\d]{3})/,{
+    message: "Insira um CEP valido"
   })
   cep?: string;
 
@@ -72,35 +96,41 @@ export class CreateClienteDto {
     description: 'O estado serve para identificar a região onde o cliente se encontra',
     example: 'SP',
   })
+  @IsString()
   estado?: string;
 
   @ApiProperty({
     description: 'A cidade serve para identificar a região onde o cliente se encontra',
     example: 'Sorocaba',
   })
+  @IsString()
   cidade?: string;
 
   @ApiProperty({
     description: 'O bairro serve para identificar o local onde o cliente se encontra',
     example: 'Vila Barão',
   })
+  @IsString()
   bairro?: string;
 
   @ApiProperty({
     description: 'A rua serve para identificar o local onde o cliente se encontra',
     example: 'Rua Manuel Lourenço Rodrigues',
   })
+  @IsString()
   rua?: string;
 
   @ApiProperty({
     description: 'O numero serve para identificar o local onde o cliente se encontra',
     example: '44',
   })
+  @IsString()
   numero?: string;
 
   @ApiProperty({
     description: 'O complemento serve para dar informações adicionais para identificar o local onde o cliente se encontra',
     example: 'apt. 42',
   })
+  @IsString()
   complemento?: string;
 }
