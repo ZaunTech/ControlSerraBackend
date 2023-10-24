6220bad17932840c683cb48d8ff1966a90e951ac
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-24 07:11:42 -0300

    Revert "feature: models para auth"
    This reverts commit 49ae0e3e991746da89a11f09ed289265df6c8404.

--- src/modules/auth/models/AuthRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { Usuario } from '../../usuarios/entities/usuario.entity';
-
-export interface AuthRequest extends Request {
-  user: Usuario;
-}
--- src/modules/auth/models/UserPayload.ts
+++
@@ -1,7 +0,0 @@
-export interface UserPayload {
-  sub: string;
-  email: string;
-  senha: string;
-  iat?: number;
-  exp?: number;
-}
