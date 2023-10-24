b60d6aaeaed1718110a67bee024811dcf7b89e05
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-24 07:12:00 -0300

    Revert "feature: resource auth"
    This reverts commit 31f40910af114bd74c04e8c91ee4edf01b64583e.

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,9 @@ import { ListaInsumosModule } from './modules/lista-insumos/lista-insumos.module
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { ProdutosBaseModule } from './modules/produtos-base/produtos-base.module';
 import { InsumosProdutosBaseModule } from './modules/insumos-produtos-base/insumos-produtos-base.module';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [InsumosModule, CategoriasModule, FornecedoresModule, ClientesModule, OrcamentosModule, PedidosModule, ProdutosModule, CotacaosModule, ListaInsumosModule, UsuariosModule, ProdutosBaseModule, InsumosProdutosBaseModule, AuthModule],
+  imports: [InsumosModule, CategoriasModule, FornecedoresModule, ClientesModule, OrcamentosModule, PedidosModule, ProdutosModule, CotacaosModule, ListaInsumosModule, UsuariosModule, ProdutosBaseModule, InsumosProdutosBaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
