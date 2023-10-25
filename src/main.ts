b5d527cbee18d5f67a2bc2243de9a40184e1ceb1
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-25 16:04:03 -0300

    update: global validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,11 +25,19 @@ async function bootstrap() {
     .addTag('produtos')
     .addTag('produtos-base')
     .addTag('usuarios')
+    .addTag('auth')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3333);
 }
 
