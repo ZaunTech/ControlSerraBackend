3addb37ab7762f151f7ff41fd2c9cec78c6dec99
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-25 16:03:06 -0300

    update: swagger auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,7 +9,10 @@ import { AuthService } from './auth.service';
 import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
+import { ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
