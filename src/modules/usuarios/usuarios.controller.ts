6da5d031b65b127bb6cb12451fd5303efce94846
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-28 23:06:17 -0300

    feature: findAllwithPagination usuarios

--- src/modules/usuarios/usuarios.controller.ts
+++ src/modules/usuarios/usuarios.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe,UsePipes, Query } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { ApiTags } from '@nestjs/swagger';
-
+import { response as res } from "express";
 @ApiTags('usuarios')
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
+  @Get('paginate')
+  async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
+    page = page;
+    perPage = perPage;
+    const totalcount = await this.usuariosService.countAll();
+    res.set('x-total-count', totalcount.toString());
+    return await this.usuariosService.findAllWithPagination(page, perPage);
+  }
+
   @Get('count')
   countAll() {
     return this.usuariosService.countAll();
   }
-
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
