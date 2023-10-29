3c438ad43d4416abb30b3ae968db169a1828a421
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-28 23:04:30 -0300

    feature: findAllwithPagination fornecedores

--- src/modules/fornecedores/fornecedores.controller.ts
+++ src/modules/fornecedores/fornecedores.controller.ts
@@ -6,17 +6,26 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { FornecedoresService } from './fornecedores.service';
 import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
 import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
 import { ApiTags } from '@nestjs/swagger';
-
+import { response as res } from "express";
 @ApiTags('fornecedores')
 @Controller('fornecedores')
 export class FornecedoresController {
   constructor(private readonly fornecedoresService: FornecedoresService) {}
 
+  @Get('paginate')
+async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
+  page = page;
+  perPage = perPage;
+  const totalcount = await this.fornecedoresService.countAllFornecedor();
+  res.set('x-total-count', totalcount.toString());
+  return await this.fornecedoresService.findAllWithPagination(page, perPage);
+}
   @Get('count')
   countAll() {
     return this.fornecedoresService.countAllFornecedor();
