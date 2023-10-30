38dfe23b0e991f14d7ca5ea4d77d719de6a403fc
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-10-30 00:34:10 -0300

    update: Auth providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,13 +16,11 @@ import { InsumosProdutosBaseModule } from './modules/insumos-produtos-base/insum
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
-import { ConfigModule } from '@nestjs/config';
+
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     InsumosModule,
     CategoriasModule,
     FornecedoresModule,
