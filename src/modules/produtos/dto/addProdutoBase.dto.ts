b552379b71fbd48041b6141579b69db1e291a41f
Divaldil <99264612+Divaldil@users.noreply.github.com>
2023-11-01 14:08:05 -0300

    validação do addprodutobase feita

--- src/modules/produtos/dto/addProdutoBase.dto.ts
+++ src/modules/produtos/dto/addProdutoBase.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, ValidateIf } from 'class-validator';
 
 export class addProdutoBaseDto {
   @ApiProperty({
-    description: 'O titulo serve para identificar o produto',
-    example: 'Portão',
+    description: 'O id serve para apontar para um produto base',
+    example: '1',
   })
+  @IsNotEmpty({message: 'O id do produto base não deve estar vazio'})
+  @IsNumber({}, {message: 'O id do produto base inserido não é válido'})
   id: number;
 
   @ApiProperty({
@@ -12,6 +15,8 @@ export class addProdutoBaseDto {
       'A quantidade serve para descrever quantas unidades deste produto serão necessárias para o orçamento',
     example: '3',
   })
+  @IsNotEmpty({message: 'A quantidade do produto base não deve estar vazio'})
+  @IsNumber({}, {message: 'A quantidade inserida não é válida'})
   quantidade: number;
 
   @ApiProperty({
@@ -19,6 +24,8 @@ export class addProdutoBaseDto {
       'As observações servem para descrever caracteristicas relevantes sobre o produto',
     example: '2" x 6 m',
   })
+  @ValidateIf((object, value) => value !== undefined)
+  @IsString({message: 'A observação inserida não é válida'})
   observacoes?: string;
 
   @ApiProperty({
@@ -26,5 +33,7 @@ export class addProdutoBaseDto {
       'O id do orçamento serve para indentificar qual o orçamento a quem este produto pertence',
     example: '1',
   })
+  @IsNotEmpty({message: 'O id do orçamento deve estar vazio'})
+  @IsNumber({}, {message: 'O id do orçamento inserido não é válido'})
   orcamentoId: number;
 }
