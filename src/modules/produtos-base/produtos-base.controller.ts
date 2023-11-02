1fd333a4ee35374478b67502b70efcaa206b5a7f
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:13:00 -0300

    update: IsPublic decorator

--- src/modules/produtos-base/produtos-base.controller.ts
+++ src/modules/produtos-base/produtos-base.controller.ts
@@ -15,49 +15,40 @@ import { CreateProdutosBaseDto } from './dto/create-produtos-base.dto';
 import { UpdateProdutosBaseDto } from './dto/update-produtos-base.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from 'express';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('produtos-base')
 @Controller('produtos-base')
 export class ProdutosBaseController {
   constructor(private readonly produtosBaseService: ProdutosBaseService) {}
 
-  @Get('paginate')
-  async findAllWithPagination(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ) {
-    page = page;
-    perPage = perPage;
-    const totalcount = await this.produtosBaseService.countAll();
-    res.set('x-total-count', totalcount.toString());
-    return await this.produtosBaseService.findAllWithPagination(page, perPage);
-  }
+  @IsPublic()
   @Get('count')
   async countAll() {
     return await this.produtosBaseService.countAll();
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createProdutosBaseDto: CreateProdutosBaseDto) {
     return await this.produtosBaseService.create(createProdutosBaseDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.produtosBaseService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.produtosBaseService.findOne(+id);
   }
-  
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateProdutosBaseDto: UpdateProdutosBaseDto) {
     return await this.produtosBaseService.update(+id, updateProdutosBaseDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.produtosBaseService.remove(+id);
