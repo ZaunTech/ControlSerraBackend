8ad02eb31bc2571fbd4f69677137484063260b23
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:08:52 -0300

    update: IsPublic decorator

--- src/modules/clientes/clientes.controller.ts
+++ src/modules/clientes/clientes.controller.ts
@@ -4,32 +4,23 @@ import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from 'express';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('clientes')
 @Controller('clientes')
 export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
 
-@Get('paginate')
-async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
-  page = page;
-  perPage = perPage;
-  const totalcount = await this.clientesService.countAllCliente();
-
-  res.set('x-total-count', totalcount.toString());
-  return await this.clientesService.findAllWithPagination(page, perPage);
-}
-
   @Get('count')
   async countAll(){
     return await this.clientesService.countAllCliente();
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createClienteDto: CreateClienteDto) {
     return await this.clientesService.create(createClienteDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.clientesService.findAll();
@@ -47,12 +38,13 @@ async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPa
     return await this.clientesService.findOne(+id);
   }
 
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
     return await this.clientesService.update(+id, updateClienteDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.clientesService.remove(+id);
