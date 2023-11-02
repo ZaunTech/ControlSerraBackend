46c38300267dde7a5efd8f1f0ae1d4b6588cdd1a
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:09:03 -0300

    update: IsPublic decorator

--- src/modules/fornecedores/fornecedores.controller.ts
+++ src/modules/fornecedores/fornecedores.controller.ts
@@ -15,30 +15,23 @@ import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
 import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from "express";
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('fornecedores')
 @Controller('fornecedores')
 export class FornecedoresController {
   constructor(private readonly fornecedoresService: FornecedoresService) {}
-
-  @Get('paginate')
-async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
-  page = page;
-  perPage = perPage;
-  const totalcount = await this.fornecedoresService.countAllFornecedor();
-  res.set('x-total-count', totalcount.toString());
-  return await this.fornecedoresService.findAllWithPagination(page, perPage);
-}
+  
   @Get('count')
   async countAll(){
     return await this.fornecedoresService.countAllFornecedor();
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() CreateFornecedoresDto: CreateFornecedorDto) {
     return await this.fornecedoresService.create(CreateFornecedoresDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.fornecedoresService.findAll();
@@ -48,13 +41,13 @@ async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPa
   async findOne(@Param('id') id: string) {
     return await this.fornecedoresService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() UpdateFornecedoresDto: UpdateFornecedorDto) {
     return await this.fornecedoresService.update(+id, UpdateFornecedoresDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.fornecedoresService.remove(+id);
