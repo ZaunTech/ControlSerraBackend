96b23d2b27fcaff53243503e986897b7d4a6788a
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:10:38 -0300

    update: IsPublic decorator

--- src/modules/insumos-produtos-base/insumos-produtos-base.controller.ts
+++ src/modules/insumos-produtos-base/insumos-produtos-base.controller.ts
@@ -4,52 +4,45 @@ import { CreateInsumosProdutosBaseDto } from './dto/create-insumo-produtos-base.
 import { UpdateInsumosProdutosBaseDto } from './dto/update-insumo-produtos-base.dto';
 import { ApiTags } from '@nestjs/swagger';
 import  {response as res} from 'express';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('insumos-produtos-base')
 @Controller('insumos-produtos-base')
 export class InsumosProdutosBaseController {
   constructor(private readonly insumosProdutosBaseService: InsumosProdutosBaseService) {}
 
-  @Get('paginate')
-async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
-  page = page;
-  perPage = perPage;
-  const totalcount = await this.insumosProdutosBaseService.countAll();
-  res.set('x-total-count', totalcount.toString());
-  return await this.insumosProdutosBaseService.findAllWithPagination(page, perPage);
-}
   @Get('count')
   async countAll() {
     return await this.insumosProdutosBaseService.countAll();
   }
-
+  @IsPublic()
   @Get('insumoProd/:id')
   findProdutoOrc(@Param('id') id: number)
   {
     return this.insumosProdutosBaseService.findInsumoProdBase(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createInsumosProdutosBaseDto: CreateInsumosProdutosBaseDto) {
     return await this.insumosProdutosBaseService.create(createInsumosProdutosBaseDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.insumosProdutosBaseService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.insumosProdutosBaseService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateInsumosProdutosBaseDto: UpdateInsumosProdutosBaseDto) {
     return await this.insumosProdutosBaseService.update(+id, updateInsumosProdutosBaseDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.insumosProdutosBaseService.remove(+id);
