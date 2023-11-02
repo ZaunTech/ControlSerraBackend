e69d2f289295002b09cbd30932cbceb310c68577
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:12:49 -0300

    update: IsPublic decorator

--- src/modules/pedidos/pedidos.controller.ts
+++ src/modules/pedidos/pedidos.controller.ts
@@ -15,59 +15,48 @@ import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from 'express';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('pedidos')
 @Controller('pedidos')
 export class PedidosController {
   constructor(private readonly pedidosService: PedidosService) {}
 
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Get('count')
   async countAll() {
     return await this.pedidosService.countAll();
   }
 
-
-  @Get('paginate')
-  async findAllWithPagination(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ) {
-    page = page;
-    perPage = perPage;
-    const totalcount = await this.pedidosService.countAll();
-    res.set('x-total-count', totalcount.toString());
-    return await this.pedidosService.findAllWithPagination(page, perPage);
-  }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createPedidoDto: CreatePedidoDto) {
     return await this.pedidosService.create(createPedidoDto);
   }
-
+  @IsPublic()
   @Get(':busca')
   async findManyByTitle(@Param('busca') buscaParam: number) {
     return await this.pedidosService.findManyByPagamento(buscaParam);
   }
 
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.pedidosService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.pedidosService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updatePedidoDto: UpdatePedidoDto) {
     return await this.pedidosService.update(+id, updatePedidoDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.pedidosService.remove(+id);
