849ac4f89bcfe217e150c983b65e6c2d07ef9d5b
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:12:41 -0300

    update: IsPublic decorator

--- src/modules/orcamentos/orcamentos.controller.ts
+++ src/modules/orcamentos/orcamentos.controller.ts
@@ -1,10 +1,10 @@
-
 import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe,UsePipes, Query } from '@nestjs/common';
 import { OrcamentosService } from './orcamentos.service';
 import { CreateOrcamentoDto } from './dto/create-orcamento.dto';
 import { UpdateOrcamentoDto } from './dto/update-orcamento.dto';
 import { ApiTags } from '@nestjs/swagger';
-import {response as res} from 'express';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
+
 @ApiTags('orcamentos')
 @Controller('orcamentos')
 export class OrcamentosController {
@@ -15,39 +15,30 @@ export class OrcamentosController {
   async countAll() {
     return await this.orcamentosService.countAll();
   }
-  
-  @Get('paginate')
-  async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
-    page = page;
-    perPage = perPage;
-    const totalcount = await this.orcamentosService.countAll();
-    res.set('x-total-count', totalcount.toString());
-    return await this.orcamentosService.findAllWithPagination(page, perPage);
-  }
 
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createOrcamentoDto: CreateOrcamentoDto) {
     return await this.orcamentosService.create(createOrcamentoDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.orcamentosService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.orcamentosService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateOrcamentoDto: UpdateOrcamentoDto) {
     return await this.orcamentosService.update(+id, updateOrcamentoDto);
-
   }
- 
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.orcamentosService.remove(+id);
