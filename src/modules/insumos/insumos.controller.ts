2ab8bcab4b8a9915a496b99daf1dc5476a365526
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:10:33 -0300

    update: IsPublic decorator

--- src/modules/insumos/insumos.controller.ts
+++ src/modules/insumos/insumos.controller.ts
@@ -15,47 +15,39 @@ import { CreateInsumoDto } from './dto/create-insumo.dto';
 import { UpdateInsumoDto } from './dto/update-insumo.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from "express";
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('insumos')
 @Controller('insumos')
 export class InsumosController {
   constructor(private readonly insumosService: InsumosService) {}
 
-  @Get('paginate')
-async findAllWithPagination(@Query('page') page: number, @Query('perPage') perPage: number) {
-  page = page;
-  perPage = perPage;
-  const totalcount = await this.insumosService.countAll();
-  res.set('x-total-count', totalcount.toString());
-  return await this.insumosService.findAllWithPagination(page, perPage);
-}
-
   @Get('count')
   async countAll() {
     return await this.insumosService.countAll();
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
    async create(@Body() createInsumoDto: CreateInsumoDto) {
     return await this.insumosService.create(createInsumoDto);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.insumosService.findAll();
   }
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.insumosService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateInsumoDto: UpdateInsumoDto) {
     return await this.insumosService.update(+id, updateInsumoDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.insumosService.remove(+id);
