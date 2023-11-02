1108ebbf2ca419d6de32ee00982f30d5a2b6b0f3
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-02 12:12:55 -0300

    update: IsPublic decorator

--- src/modules/produtos/produtos.controller.ts
+++ src/modules/produtos/produtos.controller.ts
@@ -16,33 +16,23 @@ import { UpdateProdutoDto } from './dto/update-produto.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { response as res } from 'express';
 import { addProdutoBaseDto } from './dto/addProdutoBase.dto';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
 @ApiTags('produtos')
 @Controller('produtos')
 export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
 
-  @Get('paginate')
-  async findAllWithPagination(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
-  ) {
-    page = page;
-    perPage = perPage;
-    const totalcount = await this.produtosService.countAll();
-    res.set('x-total-count', totalcount.toString());
-    return await this.produtosService.findAllWithPagination(page, perPage);
-  }
   @Get('count')
   async countAll(){
     return await this.produtosService.countAll();
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() createProdutoDto: CreateProdutoDto) {
     return await this.produtosService.create(createProdutoDto);
   }
-
+  @IsPublic()
   @Post(':idProdBase/:idOrc')
   async createProdFromBase(
     @Param('idProdBase') idProdBase: number,
@@ -50,35 +40,35 @@ export class ProdutosController {
   ) {
     return await this.produtosService.pullProdBase(+idProdBase, +idOrc);
   }
-
+  @IsPublic()
   @Get('prodOrc/:id')
   findProdutoOrc(@Param('id') id: number) {
     return this.produtosService.findProdutoOrc(+id);
   }
-
+  @IsPublic()
   @Get(':busca')
   async findManyByTitle(@Param('titulo') buscaparam: string)
   {
     return await this.produtosService.findManyByTitle(buscaparam);
   }
-
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.produtosService.findAll();
   }
 
-
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.produtosService.findOne(+id);
   }
-
+  @IsPublic()
   @UsePipes(ValidationPipe)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
     return await this.produtosService.update(+id, updateProdutoDto);
   }
-
+  @IsPublic()
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.produtosService.remove(+id);
