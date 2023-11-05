05fb9fead85827368ef18a16964dbf70759163ec
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-05 19:52:48 -0300

    update: await this.countAll produtos-base

--- src/modules/produtos-base/produtos-base.controller.ts
+++ src/modules/produtos-base/produtos-base.controller.ts
@@ -39,7 +39,7 @@ export class ProdutosBaseController {
   @Header('Access-Control-Expose-Headers', 'X-Total-Count')
   async findAll(@Query('page') page: number,@Query('perPage') perPage: number,@Query('titulo_like') titulo_like : string, @Res({ passthrough: true }) res) {
     page = page||1;
-    perPage = perPage||5;
+    perPage = perPage||await this.countAll();
     const produtos = await this.produtosBaseService.findAllWithPagination(
       page,
       Number(perPage),
