f15626df99bfdfca835ff245ca9e393b264617a0
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-11-07 19:40:27 -0300

    update: async insumos

--- src/modules/insumos/insumos.controller.ts
+++ src/modules/insumos/insumos.controller.ts
@@ -23,23 +23,20 @@ export class InsumosController {
   constructor(private readonly insumosService: InsumosService) {}
 
   @Get('count')
-  countAll() {
-    return this.insumosService.countAll();
+  async countAll() {
+    return await this.insumosService.countAll();
   }
 
   @UsePipes(ValidationPipe)
   @Post()
-  create(@Body() createInsumoDto: CreateInsumoDto) {
-    return this.insumosService.create(createInsumoDto);
+  async create(@Body() createInsumoDto: CreateInsumoDto) {
+    return await this.insumosService.create(createInsumoDto);
   }
 
   @Get()
   @Header('Access-Control-Allow-Origin', '*')
   @Header('Access-Control-Expose-Headers', 'X-Total-Count')
   async findAll(@Query('page') page: number,@Query('perPage') perPage: number,@Query('titulo_like') titulo_like : string, @Res({ passthrough: true }) res) {
-    console.log(page)
-    console.log(perPage)
-    console.log(titulo_like)
     page = page||1;
     perPage = perPage||await this.insumosService.countAll();
     const cotacoes = await this.insumosService.findAllWithPagination(
@@ -49,7 +46,7 @@ export class InsumosController {
     );
     const total = await this.insumosService.countAll(); 
     res.header('x-total-count',total);
-    return cotacoes
+    return await cotacoes
   }
 
   @Get(':id')
@@ -59,12 +56,12 @@ export class InsumosController {
 
   @UsePipes(ValidationPipe)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInsumoDto: UpdateInsumoDto) {
-    return this.insumosService.update(+id, updateInsumoDto);
+  async update(@Param('id') id: string, @Body() updateInsumoDto: UpdateInsumoDto) {
+    return await this.insumosService.update(+id, updateInsumoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.insumosService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.insumosService.remove(+id);
   }
 }
