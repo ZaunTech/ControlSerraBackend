31f7ad0a7101aa2c01f4ce699d964d4c47ca93e5
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-11-12 17:10:38 -0300

    Auth stategy and guard

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login() {
+    console.log('Login Controller');
+    // return await this.authService.login();
+  }
+}
